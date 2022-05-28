2a2fbc79a74701d4bd8e47cb46160b5bfab66b41
Jaslan <Jaslan.dlnv@gmail.com>
2022-05-28 14:20:00 +0500

    refactor: replace tags to class/id according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -33,18 +33,22 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task task-title';
+
+    listItem.className="item"
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="input checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task input text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="icon";
     deleteButton.appendChild(deleteButtonImg);
 
 
