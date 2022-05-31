8c4801b226e563319108175f9a51e14e826939c4
Jaslan <Jaslan.dlnv@gmail.com>
2022-05-31 16:15:06 +0500

    fix: add alt-attribute according to rule 1.2 in html-and-css-extended.md #2

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="list__item-delete btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='remove';
     deleteButtonImg.className="icon";
     deleteButton.appendChild(deleteButtonImg);
 
